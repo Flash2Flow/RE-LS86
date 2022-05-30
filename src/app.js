1a300f67c3285b026bd8e65d4f29336a8e636ea9
shot <shakido@mail.ru>
2022-05-30 18:19:19 +0300

    maybe_final

--- src/app.js
+++ src/app.js
@@ -18,8 +18,34 @@ $('#close-menu-auth').on('click', function(){
   $('#block_join').hide();
   return false;
 })
-$('#close-menu-reg').on('click', function(){
-  $('#block_created').hide();
+$('#close-rule-server').on('click', function(){
+  $('#up-block-rule-server').hide();
+  return false;
+})
+$('#click_rule').on('click', function(){
+  $('#up-block-rule-server').show();
+  $('#up-block-rule-join').hide();
+  $('#up-block-rule-created').hide();
+  return false;
+})
+$('#click_create').on('click', function(){
+  $('#up-block-rule-created').show();
+  $('#up-block-rule-join').hide();
+  $('#up-block-rule-server').hide();
+  return false;
+})
+$('#click_join').on('click', function(){
+  $('#up-block-rule-join').show();
+  $('#up-block-rule-created').hide();
+  $('#up-block-rule-server').hide();
+  return false;
+})
+$('#close-created-server').on('click', function(){
+  $('#up-block-rule-created').hide();
+  return false;
+})
+$('#close-join-server').on('click', function(){
+  $('#up-block-rule-join').hide();
   return false;
 })
 $('#close-menu').on('click', function(){
